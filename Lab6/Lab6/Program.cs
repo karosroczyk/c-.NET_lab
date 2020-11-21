eff6e59e3623dbb56361dbb8b7f9f50e616510f4
karosroczyk <50304244+karosroczyk@users.noreply.github.com>
2020-11-21 19:55:33 +0100

    Lab6 added

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        public static void Zad1()
+        {
+            ChristmasTree christmasTree = new ChristmasTree("Choinka", 1);
+            christmasTree.AddBauble(Color.Red, Type.Angle);
+            christmasTree.AddBauble(Color.Blue, Type.Star);
+            christmasTree.AddBauble(Color.Red, Type.Star);
+            Console.WriteLine("Jest {0} bombek w kolorze {1}.", christmasTree[Color.Red], Color.Red);
+            Console.WriteLine("Bombka o indeksie 0 w kolorze {0}.", christmasTree[0]);
+            christmasTree[0, 0] = Color.Blue;
+            Console.WriteLine("Bombka o indeksie 0 w kolorze {0}.", christmasTree[0]);
+            christmasTree.RemoveBauble(2);
+            Console.WriteLine("Jest {0} bombek w kolorze {1}.", christmasTree[Color.Red], (Color)0);
+        }
+
+        static void Zad2()
+        {
+            ChristmasTreeA christmasTreeA = new ChristmasTreeA("Choinka1", 1);
+            christmasTreeA.AddBauble(Color.Red, Type.Angle);
+            Console.WriteLine("Bombka o indeksie 0 jest w kształcie {0}.", christmasTreeA[0]);
+            Console.WriteLine("Bombka o indeksie 0 jest w kształcie {0}.", ((ChristmasTree)christmasTreeA)[0]);
+            Console.WriteLine("Bombka o indeksie 0 w kolorze {0}.", christmasTreeA.BaubleColor(0));
+        }
+        static void Zad3()
+        {
+            ChristmasTreeB christmasTreeB = new ChristmasTreeB("Choinka1", 1);
+            christmasTreeB.AddBauble(Color.Red, Type.Angle);
+            Console.WriteLine("Bombka o indeksie 0 jest w kształcie {0}.", christmasTreeB[0]);
+            Console.WriteLine("Bombka o indeksie 0 jest w kształcie {0}.", ((ChristmasTreeA)christmasTreeB)[0]);
+        }
+        static void Main(string[] args)
+        {
+            //Zad1();
+            Zad2();
+            //Zad3();
+        }
+    }
+}
---
+++ Lab6/Lab6/Zad1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab6
+{
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+    }
+    public enum Type
+    {
+        Angle,
+        Star,
+        Sphere,
+    }
+    class Tree
+    {
+        string name;
+        int age;
+
+        public Tree(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+    class Fir : Tree
+    {
+        protected List<Bauble> baubleList = new List<Bauble>();
+        public Fir(string name, int age) : base(name, age){}
+        public void AddBauble(Color baubleColor, Type baubleType)
+        {
+            baubleList.Add(new Bauble(baubleColor, baubleType));
+        }
+        public void RemoveBauble(int index)
+        {
+            baubleList.RemoveAt(index);
+        }
+        protected  class Bauble
+        {
+            public Color baubleColor { get; set; }
+            public Type baubleType { get; set; }
+            public Bauble(Color baubleColor, Type baubleType)
+            {
+                this.baubleColor = baubleColor;
+                this.baubleType = baubleType;
+            }
+        }
+    }
+    class ChristmasTree : Fir
+    {
+        public ChristmasTree(string name, int age) : base(name, age) { }
+        public virtual Object this[int index]
+        {
+            get { return baubleList[index].baubleColor; }
+        }
+        public int this[Color baubleColor]
+        {
+            get { return baubleList.Where(elem => elem.baubleColor == baubleColor).Count(); }
+        }
+        public Color this[int index, int a]
+        {
+            set => baubleList[index].baubleColor = value;
+        }
+    }
+    class ChristmasTreeA : ChristmasTree
+    {
+        public ChristmasTreeA(string name, int age) : base(name, age) { }
+        public override Object this[int index]
+        {
+            get { return base.baubleList[index].baubleType; }
+        }
+        public Object BaubleColor(int index)
+        {
+            return base[index];
+        }
+
+    }
+    class ChristmasTreeB : ChristmasTreeA
+    {
+        public ChristmasTreeB(string name, int age) : base(name, age) { }
+        public Object this[int index]
+        {
+            get { return Tuple.Create(base[index], BaubleColor(index)); }
+        }
+
+    }
+    sealed class ChristmasTreeC : ChristmasTreeA
+    {
+        public ChristmasTreeC(string name, int age) : base(name, age) { }
+    }
+    abstract class Home
+    {
+        protected abstract int Price();
+    }
+}
